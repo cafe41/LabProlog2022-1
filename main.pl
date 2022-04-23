0f2532388d4fa57b2e81b66396be7b1ac533c66b
Gustavo Vergara <gustavo.vergara.p@usach.cl>
2022-04-22 23:58:07 -0400

    Primer commit
    Creación de main, instalación de prolog e intento de aprender la sintaxis

---
+++ main.pl
@@ -0,0 +1,19 @@
+# PARADIGMAS DE LA PROGRAMACIÓN
+# SECCIÓN DEL CURSO: 0-B-2
+# PROFESOR DE TEORÍA: VICTOR FLORES
+# PROFESOR DE LABORATORIO: VICTOR FLORES
+#
+# AUTOR
+# NOMBRE: Gustavo Andrés Vergara Parra
+# RUT: 19.810.849-9
+# CARRERA: Ingeniería en Ejecución en Informática
+
+#Se puede importar en este lenguaje?
+
+#
+
+#Def de funciones
+crearCarta1(carta1).
+crearCartaN(cartaN).
+
+#No sé usar esto, ayuda
