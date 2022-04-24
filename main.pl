f5b0ab9afbed342808aef6661e0bea162c691d8a
Gustavo Vergara <gustavo.vergara.p@usach.cl>
2022-04-24 02:26:18 -0400

    Tercer commit
    intento de implementación de crearCarta1

--- main.pl
+++ main.pl
@@ -1,26 +1,40 @@
-# PARADIGMAS DE LA PROGRAMACIÓN
-# SECCIÓN DEL CURSO: 0-B-2
-# PROFESOR DE TEORÍA: VICTOR FLORES
-# PROFESOR DE LABORATORIO: VICTOR FLORES
-#
-# AUTOR
-# NOMBRE: Gustavo Andrés Vergara Parra
-# RUT: 19.810.849-9
-# CARRERA: Ingeniería en Ejecución en Informática
+% PARADIGMAS DE LA PROGRAMACIÓN
+% SECCIÓN DEL CURSO: 0-B-2
+% PROFESOR DE TEORÍA: VICTOR FLORES
+% PROFESOR DE LABORATORIO: VICTOR FLORES
+%
+% AUTOR
+% NOMBRE: Gustavo Andrés Vergara Parra
+% RUT: 19.810.849-9
+% CARRERA: Ingeniería en Ejecución en Informática
 
-#Se puede importar en este lenguaje?
+% Copypasteamos para ver la lista completa:
+% set_prolog_flag(answer_write_options,[max_depth(0)]). true.
 
-#
+% Importamos los otros archivos, creo.
+consult(baseDeDatos.pl).
+consult(tda-cardset.pl).
+consult(tda-game.pl).
 
-#Def de funciones
-elemento("0").
-elemento("a").
-elemento("b").
-elemento("c").
-elemento("d").
+%
 
-#Reglas
-crearCarta1("A" "B" "C" "D"):-
-crearCartaN("cartaN"):-
+%Base de datos
 
-#No sé usar esto, ayuda
+
+%Reglas
+% cards():-
+
+addElemento1(A, B, [_|_]):-
+    A is elemento(_).
+
+crearCarta1( [], 0, _).
+crearCarta1( [PrimE|UltE], numE, cantCartas):- 
+    addElemento1(A,B),
+    Emenos1 is numE -1,
+    PrimE is B.
+
+
+
+% crearCartaN("cartaN"):-
+
+%No sé usar esto, ayuda
